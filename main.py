17590a5712239a2559c5684d92cfd89be247f875
miyahara <50367145+subretu@users.noreply.github.com>
2021-10-22 22:49:28 +0900

    認証時の例外処理を追加

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import requests
 import json
 import datetime
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (  # 使用するモデル(イベント, メッセージ, アクションなど)を列挙
@@ -32,9 +32,11 @@ app = FastAPI()
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+
 if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
+
 if channel_access_token is None:
     print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
     sys.exit(1)
@@ -51,16 +53,19 @@ async def callback(request: Request):
 
     handler.handle(body.decode("utf-8"), signature)
 
+    # handle webhook body
+    try:
+        handler.handle(body.decode("utf-8"), signature)
+    except InvalidSignatureError:
+        raise HTTPException(status_code=400, detail="chatbot handle body error.")
+
     # LINEサーバへHTTP応答を返す
     return "ok"
 
-# Yahooより最安値取得関数
+
 def get_cheapest_price_item_yahoo(keyword):
 
     itemsearchurl = "https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch"
-
-    # 文字列をutf8に変換
-    # sentence = parse.quote(text.encode('utf-8'))
 
     # リクエストパラメーター
     params = {'Content-Type': 'application/json',
@@ -77,13 +82,10 @@ def get_cheapest_price_item_yahoo(keyword):
 
     return results['hits'][0]['name'], results['hits'][0]['url'], results['hits'][0]['price']
 
-# 楽天より最安値取得関数
+
 def get_cheapest_price_item_rakuten(keyword):
 
     itemsearchurl = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706"
-
-    # 文字列をutf8に変換
-    # sentence = parse.quote(text.encode('utf-8'))
 
     # リクエストパラメーター
     params = {'Content-Type': 'application/json',
@@ -115,20 +117,21 @@ def message_text(event):
     result_list_yahoo = get_cheapest_price_item_yahoo(umsg[0])
     result_list_rakuten = get_cheapest_price_item_rakuten(umsg[0])
 
+    message = "うーん、今の日本における最安値の商品はこれですかね。\n\n" + "商品名："
+
     if result_list_yahoo[2] < result_list_rakuten[2]:
-        reply_text = "うーん、今の日本における最安値の商品はこれですかね。\n\n" + "商品名：" + \
-            result_list_yahoo[0] + "\n" + "価格：" + \
-            str(result_list_yahoo[2]) + "\n" + "商品URL：" + result_list_yahoo[1]
+        reply_text = message + result_list_yahoo[0] + "\n価格：" + \
+            str(result_list_yahoo[2]) + "\n商品URL：" + result_list_yahoo[1]
+
     elif result_list_yahoo[2] > result_list_rakuten[2]:
-        reply_text = "うーん、今の日本における最安値の商品はこれですかね。\n\n" + "商品名：" + \
-            result_list_rakuten[0] + "\n" + "価格：" + \
-            str(result_list_rakuten[2]) + "\n" + \
-            "商品URL：" + result_list_rakuten[1]
+        reply_text = message + result_list_rakuten[0] + "\n価格：" + \
+            str(result_list_rakuten[2]) + "\n商品URL：" + result_list_rakuten[1]
+
     # 現時点ではYahooの方を返すようにしているが、ポイントを加味するようにする予定
     elif result_list_yahoo[2] == result_list_rakuten[2]:
-        reply_text = "うーん、今の日本における最安値の商品はこれですかね。\n\n" + "商品名：" + \
-            result_list_yahoo[0] + "\n" + "価格：" + \
-            str(result_list_yahoo[2]) + "\n" + "商品URL：" + result_list_yahoo[1]
+        reply_text = message + result_list_yahoo[0] + "\n価格：" + \
+            str(result_list_yahoo[2]) + "\n商品URL：" + result_list_yahoo[1]
+
     else:
         reply_text = "うーん、もう一度検索ワードを入れてもらえるかな？"
 
